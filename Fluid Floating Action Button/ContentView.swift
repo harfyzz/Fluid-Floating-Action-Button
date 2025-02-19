fcbc36e200c69cf5f7904a312dc41e71bd9736bb
Afeez <harfyzz@gmail.com>
2025-02-19 09:40:02 +0000

    Initial Commit

---
+++ Fluid Floating Action Button/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fluid Floating Action Button
+//
+//  Created by Afeez Yunus on 19/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fluid Floating Action Button/Fluid_Floating_Action_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fluid_Floating_Action_ButtonApp.swift
+//  Fluid Floating Action Button
+//
+//  Created by Afeez Yunus on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Fluid_Floating_Action_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
