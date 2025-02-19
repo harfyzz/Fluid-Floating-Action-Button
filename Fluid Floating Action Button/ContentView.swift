e2d4eb0450c3170c294dbf27e9d5f03786fa742f
Afeez <harfyzz@gmail.com>
2025-02-19 13:27:13 +0000

    -set up events

--- Fluid Floating Action Button/ContentView.swift
+++ Fluid Floating Action Button/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import RiveRuntime
 
 struct ContentView: View {
-    var fab = RiveViewModel(fileName: "menu_interaction", stateMachineName: "State Machine 1", artboardName: "Main")
+    @StateObject private var fab = fabView()
     var body: some View {
         ZStack(alignment:.leading) {
             VStack (alignment:.leading){
@@ -23,20 +23,45 @@ struct ContentView: View {
                 .fontWeight(.bold)
                 .font(.system(size: 40, weight: .bold, design: .default))
                 VStack(spacing:16){
-                    StatIconView(label: "7", image: "flame.fill", color: Color.orange, value: 17)
-                        StatIconView(label: "2", image:"heart.fill", color: Color.pink, value: 12)
+                    StatIconView(label: "7", image: "flame.fill", color: Color.orange, value: 7)
+                        StatIconView(label: "2", image:"heart.fill", color: Color.pink, value: 2)
                     
                 }
                 Spacer()
             }
-            .padding(.top)
+            .padding(.leading)
             VStack {
                 Spacer()
-                fab.view()
+                VStack{
+                    fab.view()
+                }
                     .frame(height:500)
+                    .scaleEffect(1.1, anchor: .bottomTrailing)
+                    .offset(x: 10, y:30)
+                    .onChange(of: fab.Stage1) { oldValue, newValue in
+                        let generator = UIImpactFeedbackGenerator(style: .light)
+                                        generator.impactOccurred()
+                                    
+                        print("Stage 1 is \(newValue)")
+                    }
+                    .onChange(of: fab.Stage2) { oldValue, newValue in
+                        let generator = UIImpactFeedbackGenerator(style: .light)
+                                        generator.impactOccurred()
+                        print("Stage 2 is \(newValue)")
+                    }
+                    .onChange(of: fab.Stage3) { oldValue, newValue in
+                        let generator = UIImpactFeedbackGenerator(style: .light)
+                                        generator.impactOccurred()
+                        print("Stage 3 is \(newValue)")
+                    }
+                    .onChange(of: fab.isActive) { oldValue, newValue in
+                        let generator = UIImpactFeedbackGenerator(style: .light)
+                                        generator.impactOccurred()
+                        print(newValue)
+                    }
             }
         }
-        .padding()
+        .padding(.vertical)
         .background(Color("BG"))
         .ignoresSafeArea(.all)
     }
@@ -44,6 +69,40 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+class fabView: RiveViewModel {
+    @Published var Stage1 = ""
+    @Published var Stage2 = ""
+    @Published var Stage3 = ""
+    @Published var isActive = false
+
+    init() {
+        super.init(fileName: "menu_interaction", stateMachineName: "State Machine 1", artboardName: "Main")
+    }
+    
+    func view() -> some View {
+        super.view()
+    }
+    // Subscribe to Rive events
+    @objc func onRiveEventReceived(onRiveEvent riveEvent: RiveEvent) {
+        if let generalEvent = riveEvent as? RiveGeneralEvent {
+            let eventProperties = generalEvent.properties()
+            
+            if let Stage1Value = eventProperties["Phase1"] as? String {
+                Stage1 = Stage1Value
+            }
+            if let Stage2Value = eventProperties["Phase2"] as? String {
+                Stage2 = Stage2Value
+            }
+            if let Stage3Value = eventProperties["Phase3"] as? String {
+                Stage3 = Stage3Value
+            }
+            if let isFabActive = eventProperties["isActive?"] as? Bool {
+                isActive = isFabActive
+            }
+        }
+    }
 }
 
 struct StatIconView: View {
